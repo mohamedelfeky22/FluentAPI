bdde1bef9acdb2e6126ba7f8510fc87be8ac28b3
mohamed elfeky <mohamedelfeky700@outlook.com>
2022-06-28 21:14:44 +0200

    Add project files.

---
+++ FluentAPI/Entities/StudentLogIn.cs
@@ -0,0 +1,12 @@
+﻿namespace FluentAPI.Entities
+{
+    public class StudentLogIn
+    {
+  
+        public int ID { get; set; }
+        public string EmailID { get; set; }
+        public string Password { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
